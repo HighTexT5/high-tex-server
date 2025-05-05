3c7d88066c9eb3696aebfeb7523aa41735f0ca18
CuongCevenn <21020118@vnu.edu.vn>
2025-05-06 00:40:38 +0700

    feat:get shop info

--- src/main/java/com/uet/hightex/services/common/ShopService.java
+++ src/main/java/com/uet/hightex/services/common/ShopService.java
@@ -1,7 +1,10 @@
 package com.uet.hightex.services.common;
 
+import com.uet.hightex.dtos.common.ShopInfoDto;
 import com.uet.hightex.entities.manager.BeDistributorRequest;
 
 public interface ShopService {
     void createShop(BeDistributorRequest beDistributorRequest);
+
+    ShopInfoDto getShopInfo(String shopCode);
 }
